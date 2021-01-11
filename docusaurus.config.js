9901f149035ef3ad7e4f757292783f7a159b505c
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-01-11 16:53:12 +0530

    Init

---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  title: "devMakers",
+  tagline: "Lets Become Better Devs",
+  url: "https://learn.sauravkhdoolia.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "sauravkhdoolia", // Usually your GitHub org/user name.
+  projectName: "devmakers", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "devMakers",
+      items: [
+        {
+          to: "basics/",
+          activeBasePath: "basics",
+          label: "HTML & CSS",
+          position: "right",
+        },
+        {
+          to: "js/",
+          activeBasePath: "js",
+          label: "Learn JS",
+          position: "right",
+        },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/sauravkhoolia/devmakers",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Learn",
+          items: [
+            {
+              label: "HTML & CSS",
+              to: "basics/",
+            },
+            {
+              label: "Learn JS",
+              to: "js/",
+            },
+          ],
+        },
+        {
+          title: "Track Me",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.io/devmakers",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/sauravkhdoolia",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/sauravkhdoolia/devmakers",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} devMakers, LLC. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
